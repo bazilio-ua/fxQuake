90ebecaea4ce6f1189bb4e6a13e299c9cd778f94
Basil Nikityuk <basil.nikityuk@gmail.com>
2016-12-11 22:15:18 +0200

    fixed warning C4067: unexpected tokens following preprocessor directive - expected a newline

--- glquake.h
+++ glquake.h
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 // glquake.h
 
-#ifdef __APPLE__ && __MACH__
+#if defined __APPLE__ && defined __MACH__
 #include 	<OpenGL/gl.h>
 #include	<OpenGL/glext.h>
 #include	<dlfcn.h>
