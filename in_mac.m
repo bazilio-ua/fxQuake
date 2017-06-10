21fa56b343aed66b0f977dd3a486d700056eaf83
Basil Nikityuk <basil.nikityuk@gmail.com>
2017-06-11 00:07:18 +0300

    no message

--- in_mac.m
+++ in_mac.m
@@ -41,6 +41,25 @@ qboolean vidmode_fullscreen = false; // was vidmode_active
 qboolean	vid_activewindow;
 qboolean	vid_hiddenwindow;
 
+static const byte scantokey[128] = 
+{
+/* 0x00 */    'a',            's',            'd',            'f',            'h',            'g',            'z',            'x',
+/* 0x08 */    'c',            'v',            '?',            'b',            'q',            'w',            'e',            'r',
+/* 0x10 */    'y',            't',            '1',            '2',            '3',            '4',            '6',            '5',
+/* 0x18 */    '=',            '9',            '7',            '-',            '8',            '0',            ']',            'o',
+/* 0x20 */    'u',            '[',            'i',            'p',            K_ENTER,        'l',            'j',            '\'',
+/* 0x28 */    'k',            ';',            '\\',           ',',            '/',            'n',            'm',            '.',
+/* 0x30 */    K_TAB,          K_SPACE,        '`',            K_BACKSPACE,    '?',            K_ESCAPE,       '?',            K_COMMAND,
+/* 0x38 */    K_SHIFT,        K_CAPSLOCK,     K_ALT,          K_CTRL,         '?',            '?',            '?',            '?',
+/* 0x40 */    '?',            K_KP_DEL,       '?',            K_KP_STAR,      '?',            K_KP_PLUS,      '?',            K_KP_NUMLOCK,
+/* 0x48 */    '?',            '?',            '?',            K_KP_SLASH,     K_KP_ENTER,     '?',            K_KP_MINUS,     '?',
+/* 0x50 */    '?',            '?',            K_KP_INS,       K_KP_END,       K_KP_DOWNARROW, K_KP_PGDN,      K_KP_LEFTARROW, K_KP_5,
+/* 0x58 */    K_KP_RIGHTARROW,K_KP_HOME,      '?',            K_KP_UPARROW,   K_KP_PGUP,      '?',            '?',            '?',
+/* 0x60 */    K_F5,           K_F6,           K_F7,           K_F3,           K_F8,           K_F9,           '?',            K_F11,
+/* 0x68 */    '?',            '?',            '?',            K_F14,          '?',            K_F10,          '?',            K_F12,
+/* 0x70 */    '?',            K_F15,          K_INS,          K_HOME,         K_PGUP,         K_DEL,          K_F4,           K_END,
+/* 0x78 */    K_F2,           K_PGDN,         K_F1,           K_LEFTARROW,    K_RIGHTARROW,   K_DOWNARROW,    K_UPARROW,      '?'
+};
 
 /*
  ===========
@@ -59,15 +78,9 @@ int EventToKey (NSEvent *event)
     
     characters = [event charactersIgnoringModifiers];
     characterCount = [characters length];
-    
-//    NSEventType eventType = [event type]; // DEBUG
-    
     for (characterIndex = 0; characterIndex < characterCount; characterIndex++) 
     {
         character = [characters characterAtIndex:characterIndex];
-        
-//        Con_Printf("CHARACTER:  %hu   0x%02x  down=%d\n", character, character, eventType == NSKeyDown); // DEBUG
-        
         switch (character) 
         {
         case NSPageUpFunctionKey:
@@ -348,20 +361,20 @@ void IN_ProcessEvents (void)
 
     
     
-    NSString *characters;
-    NSUInteger characterCount;
-    NSUInteger characterIndex;
-    unichar character;
+//    NSString *characters;
+//    NSUInteger characterCount;
+//    NSUInteger characterIndex;
+//    unichar character;
     
 	NSEvent *event;
-    
     while ((event = [NSApp nextEventMatchingMask:NSAnyEventMask 
                                        untilDate:[NSDate distantPast] 
                                           inMode:NSDefaultRunLoopMode 
                                          dequeue:YES])) 
     {
         NSEventType eventType = [event type];
-        switch (eventType) {
+        switch (eventType) 
+        {
 //            /* These six event types are ignored since we do all of our mouse down/up process via the uber-mouse system defined event. 
 //                We have to accept these events however since they get enqueued and the queue will fill up if we don't. */
 //            case NSLeftMouseDown:
@@ -383,28 +396,21 @@ void IN_ProcessEvents (void)
 //            case NSOtherMouseDragged:   // other mouse dragged
 //                return;
                 
-            case NSKeyDown:
-            case NSKeyUp:
-                characters = [event charactersIgnoringModifiers];
-                characterCount = [characters length];
-                for (characterIndex = 0; characterIndex < characterCount; characterIndex++) {
-                    character = [characters characterAtIndex:characterIndex];
-//                    NSLog(@"%hu", character);
-//                    Con_Printf("CHARACTER:  %hu   0x%02x  down=%d\n", character, character, eventType == NSKeyDown);
-                    
-//                    if (character < 0x80) {
-//                        if (character >= 'A' && character <= 'Z') {
-//                            character += 'a' - 'A';
-//                        }
-//                        Key_Event (character, eventType == NSKeyDown);
-//                    } else {
-//                        Key_Event (MapKey(character), eventType == NSKeyDown);
-//                    }
-                    
-                    Key_Event (character, eventType == NSKeyDown);
-                    
-                }
-//                NSLog(@"key");
+            case NSKeyDown: // key pressed
+            case NSKeyUp: // key released
+                
+                Key_Event(EventToKey(event), eventType == NSKeyDown);
+                
+//                characters = [event charactersIgnoringModifiers];
+//                characterCount = [characters length];
+//                for (characterIndex = 0; characterIndex < characterCount; characterIndex++) 
+//                {
+//                    character = [characters characterAtIndex:characterIndex];
+//                    
+//                    Key_Event (character, eventType == NSKeyDown);
+//                    
+//                }
+                
                 return;
                 
 //            case NSFlagsChanged:
