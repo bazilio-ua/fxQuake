b24ade7810fb66459199a262171b13e1d1f1ad29
Basil Nikityuk <basil.nikityuk@gmail.com>
2017-06-11 22:48:04 +0300

    keyCode to scantokey

--- in_mac.m
+++ in_mac.m
@@ -43,22 +43,22 @@ qboolean	vid_hiddenwindow;
 
 static const byte scantokey[128] = 
 {
-/* 0x00 */    'a',            's',            'd',            'f',            'h',            'g',            'z',            'x',
-/* 0x08 */    'c',            'v',            '?',            'b',            'q',            'w',            'e',            'r',
-/* 0x10 */    'y',            't',            '1',            '2',            '3',            '4',            '6',            '5',
-/* 0x18 */    '=',            '9',            '7',            '-',            '8',            '0',            ']',            'o',
-/* 0x20 */    'u',            '[',            'i',            'p',            K_ENTER,        'l',            'j',            '\'',
-/* 0x28 */    'k',            ';',            '\\',           ',',            '/',            'n',            'm',            '.',
-/* 0x30 */    K_TAB,          K_SPACE,        '`',            K_BACKSPACE,    '?',            K_ESCAPE,       '?',            K_COMMAND,
-/* 0x38 */    K_SHIFT,        K_CAPSLOCK,     K_ALT,          K_CTRL,         '?',            '?',            '?',            '?',
-/* 0x40 */    '?',            K_KP_DEL,       '?',            K_KP_STAR,      '?',            K_KP_PLUS,      '?',            K_KP_NUMLOCK,
-/* 0x48 */    '?',            '?',            '?',            K_KP_SLASH,     K_KP_ENTER,     '?',            K_KP_MINUS,     '?',
-/* 0x50 */    '?',            '?',            K_KP_INS,       K_KP_END,       K_KP_DOWNARROW, K_KP_PGDN,      K_KP_LEFTARROW, K_KP_5,
-/* 0x58 */    K_KP_RIGHTARROW,K_KP_HOME,      '?',            K_KP_UPARROW,   K_KP_PGUP,      '?',            '?',            '?',
-/* 0x60 */    K_F5,           K_F6,           K_F7,           K_F3,           K_F8,           K_F9,           '?',            K_F11,
-/* 0x68 */    '?',            '?',            '?',            K_F14,          '?',            K_F10,          '?',            K_F12,
-/* 0x70 */    '?',            K_F15,          K_INS,          K_HOME,         K_PGUP,         K_DEL,          K_F4,           K_END,
-/* 0x78 */    K_F2,           K_PGDN,         K_F1,           K_LEFTARROW,    K_RIGHTARROW,   K_DOWNARROW,    K_UPARROW,      '?'
+/* 0x00 */  'a',            's',            'd',            'f',            'h',            'g',            'z',            'x',
+/* 0x08 */  'c',            'v',             0,             'b',            'q',            'w',            'e',            'r',
+/* 0x10 */  'y',            't',            '1',            '2',            '3',            '4',            '6',            '5',
+/* 0x18 */  '=',            '9',            '7',            '-',            '8',            '0',            ']',            'o',
+/* 0x20 */  'u',            '[',            'i',            'p',            K_ENTER,        'l',            'j',            '\'',
+/* 0x28 */  'k',            ';',            '\\',           ',',            '/',            'n',            'm',            '.',
+/* 0x30 */  K_TAB,          K_SPACE,        '`',            K_BACKSPACE,    0,              K_ESCAPE,       0,              K_COMMAND,
+/* 0x38 */  K_SHIFT,        K_CAPSLOCK,     K_ALT,          K_CTRL,         0,              0,              0,              0,
+/* 0x40 */  0,              K_KP_DEL,       0,              K_KP_STAR,      0,              K_KP_PLUS,      0,              K_KP_NUMLOCK,
+/* 0x48 */  0,              0,              0,              K_KP_SLASH,     K_KP_ENTER,     0,              K_KP_MINUS,     0,
+/* 0x50 */  0,              0,              K_KP_INS,       K_KP_END,       K_KP_DOWNARROW, K_KP_PGDN,      K_KP_LEFTARROW, K_KP_5,
+/* 0x58 */  K_KP_RIGHTARROW,K_KP_HOME,      0,              K_KP_UPARROW,   K_KP_PGUP,      0,              0,              0,
+/* 0x60 */  K_F5,           K_F6,           K_F7,           K_F3,           K_F8,           K_F9,           0,              K_F11,
+/* 0x68 */  0,              0,              0,              K_F14,          0,              K_F10,          0,              K_F12,
+/* 0x70 */  0,              K_F15,          K_INS,          K_HOME,         K_PGUP,         K_DEL,          K_F4,           K_END,
+/* 0x78 */  K_F2,           K_PGDN,         K_F1,           K_LEFTARROW,    K_RIGHTARROW,   K_DOWNARROW,    K_UPARROW,      0
 };
 
 /*
@@ -398,8 +398,21 @@ void IN_ProcessEvents (void)
                 
             case NSKeyDown: // key pressed
             case NSKeyUp: // key released
-                
-                Key_Event(EventToKey(event), eventType == NSKeyDown);
+                {
+                    unsigned short vkey = [event keyCode];
+                    
+//                    if (vkey > 127)
+//                    {
+//                        return;
+//                    }
+//                    
+//                    Con_Printf("vkey: %d\n", vkey);
+                    
+                    int key = (byte)scantokey[vkey];
+                    
+                    Key_Event(key, eventType == NSKeyDown);
+                    
+//                Key_Event(EventToKey(event), eventType == NSKeyDown);
                 
 //                characters = [event charactersIgnoringModifiers];
 //                characterCount = [characters length];
@@ -410,16 +423,8 @@ void IN_ProcessEvents (void)
 //                    Key_Event (character, eventType == NSKeyDown);
 //                    
 //                }
-                
+                }   
                 return;
-                
-//            case NSFlagsChanged:
-//                return;
-//            case NSSystemDefined:
-//                return;
-//            case NSScrollWheel:
-//                return;
-                
                 
             case NSFlagsChanged: // special keys
                 {
@@ -447,7 +452,12 @@ void IN_ProcessEvents (void)
                     if (filteredFlags & NSNumericPadKeyMask)
                         Key_Event (K_NUMLOCK, (flags & NSNumericPadKeyMask) ? true : false);
                 }
-                break;
+                return;
+                
+//            case NSSystemDefined:
+//                return;
+//            case NSScrollWheel:
+//                return;
                 
                 
             default:
