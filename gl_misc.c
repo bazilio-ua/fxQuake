65fe0a46a19420467ecf71c44a9b56a8c40845d9
Basil Nikityuk <basil.nikityuk@gmail.com>
2018-11-04 18:35:43 +0200

    condition

--- gl_misc.c
+++ gl_misc.c
@@ -171,7 +171,7 @@ GL_Overbright
 */
 void GL_Overbright (void)
 {
-	if (gl_mtexable && gl_texture_env_combine && gl_texture_env_add)
+	if (gl_texture_env_combine) // overbright can't be done in a single pass without combiners
 	{
 		float m;
 		
