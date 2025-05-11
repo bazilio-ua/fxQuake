bd9b971bca07861bf278ae3ea209299c68124797
Basil Nikityuk <basil.nikityuk@gmail.com>
2025-05-11 21:16:07 +0300

    up

--- vid_cgl.m
+++ vid_cgl.m
@@ -514,7 +514,7 @@ void VID_Test (void)
 	VID_Restart ();
 	
 	//pop up confirmation dialoge
-	if (!SCR_ModalMessage("Would you like to keep this\nvideo mode? (y/n)\n", 5.0f))
+	if (!SCR_ModalMessage("Would you like to keep this\nvideo mode?\n", 5.0f))
 	{
 		//revert cvars and mode
 		Cvar_SetValue ("vid_width", old_width);
