3a2b5c8dedeeb0045890cf639ed0cd12fae34793
Basil Nikityuk <basil.nikityuk@gmail.com>
2025-06-16 13:40:40 +0300

    test for exclusive access

--- snd_mac.m
+++ snd_mac.m
@@ -31,6 +31,7 @@ AUNode mixerNode;
 AUNode converterNode;
 AudioUnit mixerUnit;
 AudioUnit converterUnit;
+AudioUnit outputUnit;
 
 // 64K is > 1 second at 16-bit, 22050 Hz
 #define	WAV_BUFFERS				64
@@ -126,96 +127,6 @@ qboolean SNDDMA_Init(void)
     snd_inited = false;
 	shm = &sn;
     
-    status = NewAUGraph(&audioGraph);
-    if (status) {
-        Con_DPrintf("NewAUGraph returned %d\n", status);
-        return false;
-    }
-    
-    status = AUGraphOpen(audioGraph);
-    if (status) {
-        Con_DPrintf("AUGraphOpen returned %d\n", status);
-        return false;
-    }
-    
-    status = AUGraphInitialize(audioGraph);
-    if (status) {
-        Con_DPrintf("AUGraphInitialize returned %d\n", status);
-        return false;
-    }
-    
-    AudioComponentDescription outputDescription;
-    outputDescription.componentType = kAudioUnitType_Output;
-    outputDescription.componentSubType = kAudioUnitSubType_DefaultOutput;
-    outputDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
-    outputDescription.componentFlags = 0;
-    outputDescription.componentFlagsMask = 0;
-    
-    status = AUGraphAddNode(audioGraph, &outputDescription, &outputNode);
-    if (status) {
-        Con_DPrintf("AUGraphAddNode returned %d\n", status);
-        return false;
-    }
-    
-    AudioComponentDescription mixerDescription;
-    mixerDescription.componentType = kAudioUnitType_Mixer;
-    mixerDescription.componentSubType = kAudioUnitSubType_StereoMixer;
-    mixerDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
-    mixerDescription.componentFlags = 0;
-    mixerDescription.componentFlagsMask = 0;
-    
-    status = AUGraphAddNode(audioGraph, &mixerDescription, &mixerNode);
-    if (status) {
-        Con_DPrintf("AUGraphAddNode returned %d\n", status);
-        return false;
-    }
-    
-    status = AUGraphConnectNodeInput(audioGraph, mixerNode, 0, outputNode, 0);
-    if (status) {
-        Con_DPrintf("AUGraphConnectNodeInput returned %d\n", status);
-        return false;
-    }
-    
-    status = AUGraphNodeInfo(audioGraph, mixerNode, 0, &mixerUnit);
-    if (status) {
-        Con_DPrintf("AUGraphNodeInfo returned %d\n", status);
-        return false;
-    }
-    
-    AudioComponentDescription converterDescription;
-    converterDescription.componentType = kAudioUnitType_FormatConverter;
-    converterDescription.componentSubType = kAudioUnitSubType_AUConverter;
-    converterDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
-    converterDescription.componentFlags = 0;
-    converterDescription.componentFlagsMask = 0;
-    
-    status = AUGraphAddNode(audioGraph, &converterDescription, &converterNode);
-    if (status) {
-        Con_DPrintf("AUGraphAddNode returned %d\n", status);
-        return false;
-    }
-    
-    status = AUGraphNodeInfo(audioGraph, converterNode, 0, &converterUnit);
-    if (status) {
-        Con_DPrintf("AUGraphNodeInfo returned %d\n", status);
-        return false;
-    }
-    
-    AURenderCallbackStruct callbackStruct;
-    callbackStruct.inputProc = &renderCallback;
-    callbackStruct.inputProcRefCon = NULL;
-    
-    status = AudioUnitSetProperty(converterUnit, 
-                                  kAudioUnitProperty_SetRenderCallback, 
-                                  kAudioUnitScope_Input, 
-                                  0, 
-                                  &callbackStruct, 
-                                  sizeof(callbackStruct));
-    if (status) {
-        Con_DPrintf("AudioUnitSetProperty returned %d\n", status);
-        return false;
-    }
-    
 	//
 	// get default device id
 	//
@@ -286,91 +197,235 @@ qboolean SNDDMA_Init(void)
 	//
 	// get default device buffer size
 	//
-	UInt32 size;
-	
-	AudioObjectPropertyAddress defaultDeviceStreamConfigurationProperty;
-	defaultDeviceStreamConfigurationProperty.mSelector = kAudioDevicePropertyStreamConfiguration;
-	defaultDeviceStreamConfigurationProperty.mScope = kAudioDevicePropertyScopeOutput;
-	defaultDeviceStreamConfigurationProperty.mElement = kAudioObjectPropertyElementMaster;
-	
-	status = AudioObjectGetPropertyDataSize(defaultDevice,
-											&defaultDeviceStreamConfigurationProperty,
-											0,
-											NULL,
-											&size);
-	if (status) {
-		Con_DPrintf("AudioObjectGetPropertyDataSize returned %d\n", status);
-		return false;
-	}
+//	UInt32 size;
+//	
+//	AudioObjectPropertyAddress defaultDeviceStreamConfigurationProperty;
+//	defaultDeviceStreamConfigurationProperty.mSelector = kAudioDevicePropertyStreamConfiguration;
+//	defaultDeviceStreamConfigurationProperty.mScope = kAudioDevicePropertyScopeOutput;
+//	defaultDeviceStreamConfigurationProperty.mElement = kAudioObjectPropertyElementMaster;
+//	
+//	status = AudioObjectGetPropertyDataSize(defaultDevice,
+//											&defaultDeviceStreamConfigurationProperty,
+//											0,
+//											NULL,
+//											&size);
+//	if (status) {
+//		Con_DPrintf("AudioObjectGetPropertyDataSize returned %d\n", status);
+//		return false;
+//	}
 	
 	//
 	// get default device buffer list
 	//
-	AudioBufferList *bufferList = NULL;
-	bufferList = (AudioBufferList *)malloc(size);
-	if (!bufferList)
-		return false;
+//	AudioBufferList *bufferList = NULL;
+//	bufferList = (AudioBufferList *)malloc(size);
+//	if (!bufferList)
+//		return false;
+//
+//	status = AudioObjectGetPropertyData(defaultDevice,
+//										&defaultDeviceStreamConfigurationProperty,
+//										0,
+//										NULL,
+//										&size,
+//										bufferList);
+//	if (status) {
+//		Con_DPrintf("AudioObjectGetPropertyData returned %d\n", status);
+//		return false;
+//	}
+//
+//	UInt32 j;
+//	UInt32 channelsPerFrame1 = 0;
+//	for (j = 0; j < bufferList->mNumberBuffers; j++) {
+//		channelsPerFrame1 += bufferList->mBuffers[j].mNumberChannels;
+//	}
+//	free(bufferList);
+	
+	//
+	// Check if device is hogged by another process
+	//
+	pid_t hoggingProcess;
+	propertySize = sizeof(pid_t);
+
+	AudioObjectPropertyAddress defaultDeviceHogModeProperty;
+	defaultDeviceHogModeProperty.mSelector = kAudioDevicePropertyHogMode;
+	defaultDeviceHogModeProperty.mScope = kAudioObjectPropertyScopeGlobal;
+	defaultDeviceHogModeProperty.mElement = kAudioObjectPropertyElementMaster;
 
 	status = AudioObjectGetPropertyData(defaultDevice,
-										&defaultDeviceStreamConfigurationProperty,
+										&defaultDeviceHogModeProperty,
 										0,
 										NULL,
-										&size,
-										bufferList);
+										&propertySize,
+										&hoggingProcess);
 	if (status) {
 		Con_DPrintf("AudioObjectGetPropertyData returned %d\n", status);
 		return false;
 	}
 
-	UInt32 j;
-	UInt32 channelsPerFrame1 = 0;
-	for (j = 0; j < bufferList->mNumberBuffers; j++) {
-		channelsPerFrame1 += bufferList->mBuffers[j].mNumberChannels;
+	if ((hoggingProcess != -1) && (hoggingProcess != getpid())) {
+		Con_Printf ("The sound device is already hogged by another application\nUnable to get exclusive access\n");
+		return false;
 	}
-	free(bufferList);
+	
+	//
+	// Take exclusive access to the device (hog mode)
+	//
+	hoggingProcess = getpid();
+	status = AudioObjectSetPropertyData(defaultDevice, &defaultDeviceHogModeProperty, 0, NULL, sizeof(pid_t), &hoggingProcess);
+	if (status) {
+		Con_DPrintf("AudioObjectSetPropertyData returned %d\n", status);
+		return false;
+	}
+	
+	
+	// AUGraph init
+    status = NewAUGraph(&audioGraph);
+    if (status) {
+        Con_DPrintf("NewAUGraph returned %d\n", status);
+        return false;
+    }
+    
+    status = AUGraphOpen(audioGraph);
+    if (status) {
+        Con_DPrintf("AUGraphOpen returned %d\n", status);
+        return false;
+    }
+    
+    status = AUGraphInitialize(audioGraph);
+    if (status) {
+        Con_DPrintf("AUGraphInitialize returned %d\n", status);
+        return false;
+    }
+    
+    AudioComponentDescription outputDescription;
+    outputDescription.componentType = kAudioUnitType_Output;
+    outputDescription.componentSubType = kAudioUnitSubType_DefaultOutput;
+    outputDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
+    outputDescription.componentFlags = 0;
+    outputDescription.componentFlagsMask = 0;
+    
+    status = AUGraphAddNode(audioGraph, &outputDescription, &outputNode);
+    if (status) {
+        Con_DPrintf("AUGraphAddNode returned %d\n", status);
+        return false;
+    }
+    
+	status = AUGraphNodeInfo(audioGraph, outputNode, NULL, &outputUnit);
+	if (status) {
+		Con_DPrintf("AUGraphNodeInfo returned %d\n", status);
+		return false;
+	}
+	
+	//
+	// Set the Current Device to the Default Output Unit
+	//
+	status = AudioUnitSetProperty(outputUnit,
+								  kAudioOutputUnitProperty_CurrentDevice,
+								  kAudioUnitScope_Global,
+								  0,
+								  &defaultDevice,
+								  sizeof(defaultDevice));
+	if (status) {
+		Con_DPrintf("AudioUnitSetProperty returned %d\n", status);
+		return false;
+	}
+	
+    AudioComponentDescription mixerDescription;
+    mixerDescription.componentType = kAudioUnitType_Mixer;
+    mixerDescription.componentSubType = kAudioUnitSubType_StereoMixer;
+    mixerDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
+    mixerDescription.componentFlags = 0;
+    mixerDescription.componentFlagsMask = 0;
+    
+    status = AUGraphAddNode(audioGraph, &mixerDescription, &mixerNode);
+    if (status) {
+        Con_DPrintf("AUGraphAddNode returned %d\n", status);
+        return false;
+    }
+    
+    status = AUGraphConnectNodeInput(audioGraph, mixerNode, 0, outputNode, 0);
+    if (status) {
+        Con_DPrintf("AUGraphConnectNodeInput returned %d\n", status);
+        return false;
+    }
+    
+    status = AUGraphNodeInfo(audioGraph, mixerNode, 0, &mixerUnit);
+    if (status) {
+        Con_DPrintf("AUGraphNodeInfo returned %d\n", status);
+        return false;
+    }
+    
+    AudioComponentDescription converterDescription;
+    converterDescription.componentType = kAudioUnitType_FormatConverter;
+    converterDescription.componentSubType = kAudioUnitSubType_AUConverter;
+    converterDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
+    converterDescription.componentFlags = 0;
+    converterDescription.componentFlagsMask = 0;
+    
+    status = AUGraphAddNode(audioGraph, &converterDescription, &converterNode);
+    if (status) {
+        Con_DPrintf("AUGraphAddNode returned %d\n", status);
+        return false;
+    }
+    
+    status = AUGraphNodeInfo(audioGraph, converterNode, 0, &converterUnit);
+    if (status) {
+        Con_DPrintf("AUGraphNodeInfo returned %d\n", status);
+        return false;
+    }
+    
+    AURenderCallbackStruct callbackStruct;
+    callbackStruct.inputProc = &renderCallback;
+    callbackStruct.inputProcRefCon = NULL;
+    
+    status = AudioUnitSetProperty(converterUnit, 
+                                  kAudioUnitProperty_SetRenderCallback, 
+                                  kAudioUnitScope_Input, 
+                                  0, 
+                                  &callbackStruct, 
+                                  sizeof(callbackStruct));
+    if (status) {
+        Con_DPrintf("AudioUnitSetProperty returned %d\n", status);
+        return false;
+    }
+    
 	
 	//
 	// use the stream format coming out of the AUHAL (should be de-interleaved)
 	//
-	AudioStreamBasicDescription streamFormat;
-	propertySize = sizeof(AudioStreamBasicDescription);
-	status = AudioUnitGetProperty(converterUnit,
-								  kAudioUnitProperty_StreamFormat,
-								  kAudioUnitScope_Output,
-								  0,
-								  &streamFormat,
-								  &propertySize);
-	if (status) {
-		Con_DPrintf("AudioUnitGetProperty returned %d\n", status);
-		return false;
-	}
+//	AudioStreamBasicDescription streamFormat;
+//	propertySize = sizeof(AudioStreamBasicDescription);
+//	status = AudioUnitGetProperty(converterUnit,
+//								  kAudioUnitProperty_StreamFormat,
+//								  kAudioUnitScope_Output,
+//								  0,
+//								  &streamFormat,
+//								  &propertySize);
+//	if (status) {
+//		Con_DPrintf("AudioUnitGetProperty returned %d\n", status);
+//		return false;
+//	}
 	
 	//
 	// check the input device's stream format
 	//
-	AudioStreamBasicDescription deviceFormat;
-	status = AudioUnitGetProperty(converterUnit,
-								  kAudioUnitProperty_StreamFormat,
-								  kAudioUnitScope_Input,
-								  0,
-								  &deviceFormat,
-								  &propertySize);
-	if (status) {
-		Con_DPrintf("AudioUnitGetProperty returned %d\n", status);
-		return false;
-	}
-
+//	AudioStreamBasicDescription deviceFormat;
+//	status = AudioUnitGetProperty(converterUnit,
+//								  kAudioUnitProperty_StreamFormat,
+//								  kAudioUnitScope_Input,
+//								  0,
+//								  &deviceFormat,
+//								  &propertySize);
+//	if (status) {
+//		Con_DPrintf("AudioUnitGetProperty returned %d\n", status);
+//		return false;
+//	}
+//	
+//	printf ("device rate %f, graph rate %f\n",
+//			deviceFormat.mSampleRate,
+//			streamFormat.mSampleRate);
 	
 	
-	printf ("device rate %f, graph rate %f\n",
-			deviceFormat.mSampleRate,
-			streamFormat.mSampleRate);
-	
-	
-	
-	
-	
-
 /*
 	// Tell the main app what we expect from it
 	// sound speed
