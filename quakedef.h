147c33acf6c1efa9702d72550c8da9b64f7d072a
Basil Nikityuk <basil.nikityuk@gmail.com>
2025-07-04 16:53:14 +0300

    quakedef update

--- quakedef.h
+++ quakedef.h
@@ -229,8 +229,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "sbar.h"
 #include "sound.h"
 #include "render.h"
-#include "client.h"
 #include "progs.h"
+#include "client.h"
 #include "server.h"
 
 #include "model.h"
