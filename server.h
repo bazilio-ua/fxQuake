eba12f6df21da90aa86471c19926cd70ee79a274
Basil Nikityuk <basil.nikityuk@gmail.com>
2025-12-17 13:16:58 +0200

    up

--- server.h
+++ server.h
@@ -66,7 +66,7 @@ typedef struct
 	byte		reliable_datagram_buf[MAX_DATAGRAM];
 
 	sizebuf_t	signon;
-	byte		signon_buf[MAX_MSGLEN-2]; //8192
+	byte		signon_buf[MAX_MSGLEN-2]; //johnfitz -- was 8192, now uses MAX_MSGLEN
 
 	int			protocol;
 	unsigned	protocolflags;
