6c592e9211732665ab5df2c679b510194bc59df4
Basil Nikityuk <basil.nikityuk@gmail.com>
2026-04-15 11:40:42 +0300

    bump MAX_MOD_KNOWN to 4096

--- gl_model.c
+++ gl_model.c
@@ -38,7 +38,7 @@ static int	mod_novis_capacity;
 static byte	*mod_decompressed;
 static int	mod_decompressed_capacity;
 
-#define	MAX_MOD_KNOWN	2048 // was 512
+#define	MAX_MOD_KNOWN	4096 // was 512
 model_t	mod_known[MAX_MOD_KNOWN];
 int		mod_numknown;
 
