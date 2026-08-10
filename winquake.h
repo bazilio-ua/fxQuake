515107a57f5782980d177f3adef32f9a864a1cfa
Basil N <bazilio@>
2026-08-10 20:58:47 +0300

    dinput version

--- winquake.h
+++ winquake.h
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #ifndef DIRECTINPUT_VERSION
-#define DIRECTINPUT_VERSION         0x0300
+#define DIRECTINPUT_VERSION         0x0700
 #endif
 
 #include <windows.h>
